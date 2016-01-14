6e0cfada1778ddd5ed5a16c9793b363c98d48431
grainnecasey <g_r_casey@live.com>
2016-01-13 20:36:57 -0500

    start of drive subsystem and such

---
+++ 811RobotCode2016/src/org/usfirst/frc/team811/robot/Config.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team811.robot;
+
+public interface Config {
+	
+	//ports
+	
+	int FRONT_RIGHT_PORT = 1;
+	int FRONT_LEFT_PORT = 1;
+	int BACK_RIGHT_PORT = 1;
+	int BACK_LEFT_PORT = 1;
+	
+	
+
+}
--- 811RobotCode2016/src/org/usfirst/frc/team811/robot/RobotMap.java
+++ 811RobotCode2016/src/org/usfirst/frc/team811/robot/RobotMap.java
@@ -1,11 +1,17 @@
 package org.usfirst.frc.team811.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
+public class RobotMap implements Config {
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
@@ -15,4 +21,28 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	
+	public static SpeedController drivefrontright;
+    public static SpeedController drivefrontleft;
+    public static SpeedController drivebackleft;
+    public static SpeedController drivebackright;
+    
+    public static RobotDrive driveRobotDrive41;
+    
+    public static void init() {
+    	
+    	drivefrontright = new Talon(FRONT_RIGHT_PORT);
+        LiveWindow.addActuator("Drive", "frontright", (Talon) drivefrontright);
+        
+        drivefrontleft = new Talon(FRONT_LEFT_PORT);
+        LiveWindow.addActuator("Drive", "frontleft", (Talon) drivefrontleft);
+        
+        drivebackleft = new Talon(BACK_LEFT_PORT);
+        LiveWindow.addActuator("Drive", "backleft", (Talon) drivebackleft);
+        
+        drivebackright = new Talon(BACK_RIGHT_PORT);
+        LiveWindow.addActuator("Drive", "backright", (Talon) drivebackright);
+        
+        
+    }
 }
