79d703fcd47619c0d8e9ef50bef11d0796ea62e8
Musti Eyceoz <maxusmusti@gmail.com>
2016-02-03 19:37:16 -0500

    Finished DriveAuto (I think)

--- 811RobotCode2016/src/org/usfirst/frc/team811/robot/commands/drive_auto.java
+++ 811RobotCode2016/src/org/usfirst/frc/team811/robot/commands/drive_auto.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team811.robot.commands;
 
+import org.usfirst.frc.team811.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,6 +12,7 @@ public class drive_auto extends Command {
     public drive_auto() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.drive);
     }
 
     // Called just before this Command runs the first time
@@ -18,6 +21,7 @@ public class drive_auto extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.drive.driveAuto(76.75);
     }
 
     // Make this return true when this Command no longer needs to run execute()
