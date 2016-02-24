33dee3dc62d4bc31291e275d82e0554b8b2834bb
Pazu <lightnesshaku@gmail.com>
2016-02-23 19:57:22 -0500

    distance shoot timeout

--- 811RobotCode2016/src/org/usfirst/frc/team811/robot/commands/shoot_distanceshoot.java
+++ 811RobotCode2016/src/org/usfirst/frc/team811/robot/commands/shoot_distanceshoot.java
@@ -16,6 +16,7 @@ public class shoot_distanceshoot extends Command {
     	
     	requires(Robot.tracker);
     	requires(Robot.shooter);
+    	setTimeout(3);
     }
 
     // Called just before this Command runs the first time
@@ -30,7 +31,7 @@ public class shoot_distanceshoot extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
